5fd9511f60ff40d9fd1b77e05b35224515027e2c
rafapitzer <rafa_thadeupitzer@hotmail.com>
2018-11-17 09:54:26 -0200

    inclusao de classes

---
+++ src/entities/Post.java
@@ -0,0 +1,77 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Post {
+	
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	
+	private Date moment;
+	private String title;
+	private String content;
+	private Integer like;
+	
+	private List<Comment> comments = new ArrayList<>();
+	
+	public Post() {
+	}
+	
+	public Post(Date moment, String title, String content, Integer like) {
+		this.moment = moment;
+		this.title = title;
+		this.content = content;
+		this.like = like;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getLike() {
+		return like;
+	}
+
+	public void setLike(Integer like) {
+		this.like = like;
+	}
+
+	public List<Comment> getComment() {
+		return comments;
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append( "title = " + title + "/n" );
+		sb.append( like + "likes - " );
+		sb.append( sdf.format( moment ) + "/n");
+		sb.append( content + "/n");
+		sb.append( "Comments: /n");
+		for ( Comment c : comments ) {
+			sb.append( c.getText() + "/n");
+		}
+		return sb.toString();
+	}
+}
